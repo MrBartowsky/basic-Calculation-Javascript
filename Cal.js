0d6b5e28aff6cc7b10c22671ff32b2c2ae33d1d9
MrBartowsky <ajayisamuel2080@gmai.com>
2024-10-08 20:00:47 +0100

    first commit

---
+++ Cal.js
@@ -0,0 +1,52 @@
+alert("welcome")
+
+var myName = "My name is Samuel";
+document.write(myName= "My name is samuel")
+document.write("<br>")
+
+var myName = "Basic calculation in Js";
+document.write(myName= "Basic calculation in Js")
+document.write("<br>")
+
+
+document.write("<br>")
+var a = "10"
+var b = "20"
+document.write(a*b);
+document.write("<br>")
+
+document.write("<br>")
+var c = "100"
+var d = "20"
+document.write(c/d);
+document.write("<br>")
+
+document.write("<br>")
+var c = "25"
+var d = "20"
+document.write(c-d);
+document.write("<br>")
+
+document.write("<br>")
+var c = "25"
+var d = "20"
+document.write(c+d);
+document.write("<br>")
+
+document.write("<br>")
+var a = "20"
+var b = "5"
+document.write(a**b);
+document.write("<br>")
+
+document.write("<br>")
+var c = "60"
+var d = "20"
+document.write(c/d);
+document.write("<br>")
+
+document.write("<br>")
+var c = "90"
+var d = "3"
+document.write(c-d);
+document.write("<br>")
